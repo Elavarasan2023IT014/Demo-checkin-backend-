1014065989b2583bfabf03758d4dfaf5bdf3f046
Elavarasan2023IT014 <elavarasan.r2023it@sece.ac.in>
2025-04-10 21:24:00 +0530

    save

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/api/login', async (req, res) => {
     return res.status(401).json({ message: 'Invalid credentials' });
   }
   const token = jwt.sign({ employeeId }, JWT_SECRET, { expiresIn: '30d' });
-  res.json({ message: 'Login successful', token });
+  res.json({ message: 'Login successful', token , employeeId: employee.employeeId });
 });
 
 // Auto check-in endpoint with notification
